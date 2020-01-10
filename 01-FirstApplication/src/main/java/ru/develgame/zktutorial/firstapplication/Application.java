5c0a1a78ea633724c116debf1af0b962138f23f0
Ilya <pascal.ilya@gmail.com>
2020-01-10 13:34:30 +0300

    First Application

---
+++ 01-FirstApplication/src/main/java/ru/develgame/zktutorial/firstapplication/Application.java
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla
+ * Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2019 Ilya Zemskov */
+package ru.develgame.zktutorial.firstapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
